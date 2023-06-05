f02283a04e9605ec5b1e1550ff9cdbdaf4cfe132
Luka Potočnik <67372390+lpoto@users.noreply.github.com>
2023-06-05 07:14:59 +0200

    Update python generator (#75)

--- lua/telescope-tasks/generators/default/python.lua
+++ lua/telescope-tasks/generators/default/python.lua
@@ -13,13 +13,15 @@ local python = Default:new {
   },
 }
 
+local get_binary
+
 function python.generator(buf)
   local filetype = vim.api.nvim_buf_get_option(buf, "filetype")
   if filetype ~= "python" then
     return nil
   end
   local name = vim.api.nvim_buf_get_name(buf)
-  local binary = util.get_binary "python" or "python"
+  local binary = get_binary()
 
   local cmd = {
     binary,
@@ -41,12 +43,30 @@ function python.generator(buf)
   return t
 end
 
-function python.healthcheck()
-  local binary = util.get_binary "python" or "python"
+function get_binary()
+  local binary = util.get_binary "python"
+  if type(binary) ~= "string" then
+    binary = "python"
+    if vim.fn.executable(binary) == 0 and vim.fn.executable "python3" == 1 then
+      return "python3",
+        nil,
+        "'python' is not executable, using 'python3' instead"
+    end
+  end
   if vim.fn.executable(binary) == 0 then
-    vim.health.warn("Python binary '" .. binary .. "' is not executable", {
+    return binary, "'" .. binary .. "' is not executable", nil
+  end
+  return binary
+end
+
+function python.healthcheck()
+  local binary, err, warn = get_binary()
+  if err ~= nil then
+    vim.health.warn(err, {
       "Install 'python' or set a different binary with vim.g.telescope_tasks = { binaries = { python=<new-binary> }}",
     })
+  elseif warn ~= nil then
+    vim.health.warn(warn)
   else
     vim.health.ok("'" .. binary .. "' is executable")
   end
